9f680bee83eae33f3e85dc3df15229c48e5053eb
stephenjtyrrell <stetyrrell132@hotmail.com>
2022-07-16 19:10:02 +0100

    Initial Commit

---
+++ DataModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrillSolution
+{
+    public class Menu
+    {
+        public string Id { get; set; }
+        public string menu { get; set; }
+        public List<MenuItem> items { get; set; }
+    }
+
+    public class MenuItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public string Duration { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using GrillSolution;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        List<Menu> Menus = getMenus();
+        List<Menu> SortedMenus = Menus.OrderBy(m=>m.menu).ToList();
+        foreach(Menu fullMenu in SortedMenus){
+            List<MenuItem> menuItems = fullMenu.items;
+            Console.WriteLine(fullMenu.menu + " has " + menuItems.Count + " types of items");
+            int quantityOfItems = new int();
+            foreach(MenuItem menuItem in menuItems){
+                Console.WriteLine(menuItem.Name + " has a quantity of " + menuItem.Quantity);
+                quantityOfItems = quantityOfItems + menuItem.Quantity;
+            }
+            
+             Console.WriteLine(fullMenu.menu + " has " + quantityOfItems + " total items");
+           
+        }
+
+    }
+
+    static List<Menu> getMenus()
+    {
+        List<Menu> Menus = new List<Menu>();
+        var client = new RestClient("http://isol-grillassessment.azurewebsites.net/api/");
+        var request = new RestRequest("GrillMenu");
+        var response = client.Execute(request);
+        if(response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            var MenuResponse = response.Content;
+            Menus = JsonConvert.DeserializeObject<List<Menu>>(MenuResponse);
+        }
+        return Menus;
+    }
+}
+
+
